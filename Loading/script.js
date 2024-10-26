74e581216685666778d200b95ff604379dcb2ccb
happpycorn <happpy.corn.man@gmail.com>
2024-10-26 18:02:02 +0800

    change logic

--- Loading/script.js
+++ Loading/script.js
@@ -1,8 +1,42 @@
-window.onload = function() {
+const url = "https://script.google.com/macros/s/AKfycbwlDDXSn4oyUNEAmzAayWN9bljmOkOBPjerBc3Os1jDLN-GVjgxZlxgL9GUXvkDTg/exec";
+const grade = ['國三', '國二', '國一', '高三', '高一', '老師', '範本'];
 
+function getQueryParams() {
     const params = new URLSearchParams(window.location.search);
-    const gradename = params.get("gradename");
-    const number = params.get("number");
+    return {
+        gradename: params.get('gradename'),
+        number: params.get('number')
+    };
+}
 
-    window.location.href = `/2024-Fhehs-Run/ScoreSearch?gradename=${gradename}&number=${number}`;
+window.onload = async function() {
+
+    const params = getQueryParams();
+
+    const gradename = grade[parseInt(params.gradename)];
+    
+    try {
+
+        const response = await fetch(`${url}?name=${gradename}`);
+        const data = await response.json();
+        
+        let found = false;
+
+        data.forEach(entry => {
+            if (entry[0] === params.number) {
+                window.location.href = `/2024-Fhehs-Run/ScoreSearch?number=${entry[0]}&name=${entry[1]}&score=${entry[6]}`;
+                found = true;
+            }
+        });
+
+        if (!found) {
+            displayNoResult();
+        }
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        displayNoResult();
+    }
+
+    
 };
